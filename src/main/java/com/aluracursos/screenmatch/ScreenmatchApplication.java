af1e669f30a0407992df715e06425465acef9a6a
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-18 17:54:14 -0500

    Agregando otra API para probar ConsumoAPI

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -15,7 +15,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         var consumoApi = new ConsumoAPI();
-        var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=bones&apikey=ba29c8e");
+        //var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=bones&apikey=ba29c8e");
+        var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
         System.out.println(json);
     }
 }
