8d10d8165732e4b2eb54c7c82d0405df9db78f3f
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-18 17:30:36 -0500

    Agregando var consumoAPI

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -13,6 +13,6 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        var consumoAPI = new ConsumoAPI();
     }
 }
