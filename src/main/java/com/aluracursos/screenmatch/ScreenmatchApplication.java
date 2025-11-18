e09af159adf3fd91f4091298fb7deeb08c33afea
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-18 17:34:10 -0500

    Correción consumoAPI por consumoApi

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -14,6 +14,6 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var consumoAPI = new ConsumoAPI();
+        var consumoApi = new ConsumoAPI();
     }
 }
