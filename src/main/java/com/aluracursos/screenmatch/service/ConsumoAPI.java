92f211c0bb41c3d2edb82445b0a51d67678b4313
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-18 13:38:31 -0500

    Agregando clase HttpResponse

--- src/main/java/com/aluracursos/screenmatch/service/ConsumoAPI.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsumoAPI.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch.service;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConsumoAPI {
     public String obtenerDatos(String url) {
