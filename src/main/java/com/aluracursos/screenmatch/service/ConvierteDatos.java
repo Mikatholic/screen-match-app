48043186cc4f6ad608381dff6d499bd749fb9ab0
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-19 13:20:29 -0500

    Agregando metodo para obtenerDatos

--- src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
@@ -4,4 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConvierteDatos implements IConvierteDatos {
     private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        return null;
+    }
 }
