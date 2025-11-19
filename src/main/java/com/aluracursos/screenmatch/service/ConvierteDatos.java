5f9d1a79942fc9edb6102292c79ec9a30703b4ea
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-19 13:07:38 -0500

    Agregando clase IConvierteDatos

--- src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
@@ -2,6 +2,6 @@ package com.aluracursos.screenmatch.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class ConvierteDatos {
+public class ConvierteDatos implements IConvierteDatos {
     private ObjectMapper objectMapper = new ObjectMapper();
 }
