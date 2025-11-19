e11dd135f7450fb0fc181b5fed11a620d7d463dd
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-19 12:34:22 -0500

    Agregando instancia con la variable ObjectMapper

--- src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
@@ -1,4 +1,7 @@
 package com.aluracursos.screenmatch.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ConvierteDatos {
+    private ObjectMapper objectMapper = new ObjectMapper();
 }
