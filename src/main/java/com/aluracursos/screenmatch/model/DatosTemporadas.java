3f33e4eb0a920e1ae837672b701ccb3e85235bd2
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-19 19:51:27 -0500

    Agregando anotación para ignorar aquellos campos que no hemos mapeado dentro de esta clase.

--- src/main/java/com/aluracursos/screenmatch/model/DatosTemporadas.java
+++ src/main/java/com/aluracursos/screenmatch/model/DatosTemporadas.java
@@ -1,7 +1,10 @@
 package com.aluracursos.screenmatch.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosTemporadas(
         Integer numero,
         List<DatosEpisodio> episodios
