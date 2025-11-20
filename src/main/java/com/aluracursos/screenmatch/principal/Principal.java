e15401716e2cb10f0b9259fc4a55aacccbadc1be
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-20 10:31:08 -0500

    Agregando instancia de la clase ConsumoAPI declarando la variable con la palabra consumoAPI

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
 
     public void muestraElMenu() {
         System.out.println("Por favor escribe el nombre de la serie que deseas buscar");
