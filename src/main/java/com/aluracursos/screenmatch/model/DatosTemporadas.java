d8177be3bbaf2b155bb695c155528ed80df36e85
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-19 19:48:37 -0500

    Agregando clase java.util

--- src/main/java/com/aluracursos/screenmatch/model/DatosTemporadas.java
+++ src/main/java/com/aluracursos/screenmatch/model/DatosTemporadas.java
@@ -1,7 +1,9 @@
 package com.aluracursos.screenmatch.model;
 
+import java.util.List;
+
 public record DatosTemporadas(
         Integer numero,
-        List <DatosEpisodio> episodios
+        List<DatosEpisodio> episodios
 ) {
 }
