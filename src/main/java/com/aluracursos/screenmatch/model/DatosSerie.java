6fea144f578638aadaffa8eb15721f95be970995
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-19 21:02:40 -0500

    Corrigiendo el "totalSeasons " por "totalSeasons"

--- src/main/java/com/aluracursos/screenmatch/model/DatosSerie.java
+++ src/main/java/com/aluracursos/screenmatch/model/DatosSerie.java
@@ -6,6 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosSerie(
         @JsonAlias ("Title") String titulo,
-        @JsonAlias ("totalSeasons ") Integer totalDeTemporadas,
+        @JsonAlias ("totalSeasons") Integer totalDeTemporadas,
         @JsonAlias ("imdbRating") String evaluacion ) {
 }
