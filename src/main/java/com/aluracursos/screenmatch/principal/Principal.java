d9450f8f938145d959a491ae5dd2a40073c26d77
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-20 10:03:54 -0500

    Agregando sout para imprimir un mensaje

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -2,6 +2,6 @@ package com.aluracursos.screenmatch.principal;
 
 public class Principal {
     public void muestraElMenu() {
-
+        System.out.println("Por favor escribe el nombre de la serie que deseas buscar");
     }
 }
