2cf10a6ffadced98aca26fd0e9871194ec929eb6
Mikatholic <miguelangelferroescalante@gmail.com>
2025-11-20 10:35:43 -0500

    Corrigiendo nombre de la variable.

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
-    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConsumoAPI consumoApi = new ConsumoAPI();
 
     public void muestraElMenu() {
         System.out.println("Por favor escribe el nombre de la serie que deseas buscar");
         var nombreSerie = teclado.nextLine();
+        var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=bones&apikey=ba29c8e");
+
     }
 }
